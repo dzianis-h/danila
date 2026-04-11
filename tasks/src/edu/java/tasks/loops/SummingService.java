89fcf88f772c75a3bc8b9c5976b2c97381801e1e
Dzianis Haurylavets <dzianis.haurylavets@gmail.com>
2026-04-11 19:06:32 +0300

    add summing task

---
+++ tasks/src/edu/java/tasks/loops/SummingService.java
@@ -0,0 +1,9 @@
+package edu.java.tasks.loops;
+
+public class SummingService {
+
+    // todo: calc sum of the all elements in array
+    public double calcSum(double[] array) {
+        return -1;
+    }
+}
