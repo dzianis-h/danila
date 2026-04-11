038f233c4873da342570413b1ded0bd1faf02ba0
reheheveba <danikponomorev@gmail.com>
2026-04-11 14:47:20 +0200

    idk

--- tasks/src/edu/java/tasks/loops/DistanceService.java
+++ tasks/src/edu/java/tasks/loops/DistanceService.java
@@ -1,4 +1,6 @@
 package edu.java.tasks.loops;
+import java.lang.*;
+import java.lang.reflect.Array;
 
 public class DistanceService {
 
@@ -8,8 +10,14 @@ public class DistanceService {
     // от начала до конца вектора по каждой из осей координат.
     // Если массив пустой, то длина вектора равна нулю.
     public double calcLength(double[] vector) {
-        return -1; // todo
 
+        double x = calcDistance(1,1);
+        double y = calcDistance(1,1);
+        double vectorLength = Math.sqrt((x + y)*(x + y));
+        while (vector = 0){
+            vector.length = 0;
+        }
+        return vectorLength;
     }
 
     // посчитать расстояние между двумя точками
@@ -17,6 +25,10 @@ public class DistanceService {
     // если у векторов разная длина, то расстояние равно нулю
     // если оба вектора нулевой длины, то расстояние тоже равно нулю
     public double calcDistance(double[] a, double[] b) {
+        double distance = (a[5]) - (b[2]);
+        if (distance < 0){
+
+        }
         return -1; // todo
     }
 }
